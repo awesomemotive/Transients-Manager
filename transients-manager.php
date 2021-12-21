69dd863014b5ca446f0dfb930c46f1ddac645718
John James Jacoby <johnjamesjacoby@me.com>
2021-12-21 15:45:46 -0600

    Bump versions to 2.0.1.

--- transients-manager.php
+++ transients-manager.php
@@ -11,7 +11,7 @@
  * Text Domain:       transients-manager
  * Requires PHP:      5.6.20
  * Requires at least: 5.3
- * Version:           2.0.0
+ * Version:           2.0.1
  */
 
 // Exit if accessed directly
